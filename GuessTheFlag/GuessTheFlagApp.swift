bb5a7063ff4bc1648a00ccc3e1dc09792d7bbbba
SeongYong_Song <songsy0612@gmail.com>
2025-09-11 20:57:13 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by SeongYongSong on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
